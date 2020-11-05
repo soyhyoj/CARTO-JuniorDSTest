b8fbc93e376f7bc119e8f4c8b6d73a7147126aed
Hyojung Lee <soyhyoj@gmail.com>
2020-11-05 21:41:21 +0100

    logitude-latitude for taxi trip assigned as Points and saved in a new table called taxi_geom

---
+++ notebooks/2.make_geometry.sql
@@ -0,0 +1,53 @@
+########################################################################################
+# create a new table called 'taxi_geom' which contains (long, lat) as geometry attribute
+
+with all_geom as
+	(
+	with
+	jan_geom as
+	(
+		select
+			tpep_pickup_datetime,
+			tpep_dropoff_datetime,
+			trip_distance,
+			ST_MakePoint(pickup_longitude, pickup_latitude) as pickup_point,
+			ST_MakePoint(dropoff_longitude, dropoff_latitude) as dropoff_point
+		from taxi_jan
+	),
+	
+	apr_geom as
+	(
+		select
+			tpep_pickup_datetime,
+			tpep_dropoff_datetime,
+			trip_distance,
+			ST_MakePoint(pickup_longitude, pickup_latitude) as pickup_point,
+			ST_MakePoint(dropoff_longitude, dropoff_latitude) as dropoff_point
+		from taxi_apr
+	),
+	
+	jul_geom as
+	(
+		select
+			tpep_pickup_datetime,
+			tpep_dropoff_datetime,
+			trip_distance,
+			ST_MakePoint(pickup_longitude, pickup_latitude) as pickup_point,
+			ST_MakePoint(dropoff_longitude, dropoff_latitude) as dropoff_point
+		from taxi_jul
+	)
+	
+	select * from jan_geom 
+	union
+	select * from apr_geom
+	union
+	select * from jul_geom
+	
+	)
+
+select *
+into table taxi_geom
+from all_geom
+
+# end of query
+#######################################################################################
