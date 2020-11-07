18c435b23371d30edf63e6d3fc0f2851fda0e0c4
Hyojung Lee <soyhyoj@gmail.com>
2020-11-07 16:51:14 +0100

    query changed to separate each month data and to select points within census blocks only

--- notebooks/2.make_geometry.sql
+++ notebooks/2.make_geometry.sql
@@ -1,50 +1,50 @@
-########################################################################################
-# create a new table called 'taxi_geom' which contains (long, lat) as geometry attribute
 
-with all_geom as
-	(
-	with
+drop table if exists pickup_blocks_jan
+
+#######################################################################################
+# get geometry of pickup points using (long, lat)
+# create a spatially joined table using pickup points and census block polygons
+
+create table pickup_blocks_jan as
+with
 	jan_geom as
 	(
 		select
-			tpep_pickup_datetime,
-			tpep_dropoff_datetime,
-			ST_SetSRID(ST_MakePoint(pickup_longitude, pickup_latitude), 4326) as pickup_point,
-			ST_SetSRID(ST_MakePoint(dropoff_longitude, dropoff_latitude), 4326) as dropoff_point
+			tpep_pickup_datetime as pickup_time,
+			ST_SetSRID(ST_MakePoint(pickup_longitude, pickup_latitude), 4326) as pickup_point
 		from taxi_jan
-	),
-	
+	)
+select jan.*, census.*
+from jan_geom as jan, census_blocks as census
+where ST_Contains(census.geometry, jan.pickup_point);
+
+
+create table pickup_blocks_apr as
+with
 	apr_geom as
 	(
 		select
-			tpep_pickup_datetime,
-			tpep_dropoff_datetime,
-			ST_SetSRID(ST_MakePoint(pickup_longitude, pickup_latitude), 4326) as pickup_point,
-			ST_SetSRID(ST_MakePoint(dropoff_longitude, dropoff_latitude), 4326) as dropoff_point
+			tpep_pickup_datetime as pickup_time
+			ST_SetSRID(ST_MakePoint(pickup_longitude, pickup_latitude), 4326) as pickup_point
 		from taxi_apr
-	),
-	
+	)
+select apr.*, census.*
+from apr_geom as apr, census_blocks as census
+where ST_Contains(census.geometry, apr.pickup_point);
+
+
+create table pickup_blocks_jul as
+with
 	jul_geom as
 	(
 		select
-			tpep_pickup_datetime,
-			tpep_dropoff_datetime,
-			ST_SetSRID(ST_MakePoint(pickup_longitude, pickup_latitude), 4326) as pickup_point,
-			ST_SetSRID(ST_MakePoint(dropoff_longitude, dropoff_latitude), 4326) as dropoff_point
+			tpep_pickup_datetime as pickup_time
+			ST_SetSRID(ST_MakePoint(pickup_longitude, pickup_latitude), 4326) as pickup_point
 		from taxi_jul
 	)
-	
-	select * from jan_geom 
-	union
-	select * from apr_geom
-	union
-	select * from jul_geom
-	
-	)
-
-select *
-into table taxi_geom
-from all_geom
+select jul.*, census.*
+from jul_geom as jul, census_blocks as census
+where ST_Contains(census.geometry, jul.pickup_point);
 
 # end of query
-#######################################################################################
+###################################################################################################
